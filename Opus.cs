de21756e342b9840d624fdbe05f3c5a91e2a620f
Frooxius <tomas.mariancik@gmail.com>
2023-10-27 18:54:57 -0500

    Upgrade solution & lib

---
+++ Opus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace POpusCodec
+{
+    public static class Opus
+    {
+        public static string Version
+        {
+            get
+            {
+                var ptr = Wrapper.opus_get_version_string();
+                return Marshal.PtrToStringAnsi(ptr);
+            }
+        }
+    }
+}
